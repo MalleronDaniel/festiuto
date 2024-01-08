edc640fa26d9b365691ded8fbb6e7dcf09bb49d6
Adam.Daniel <adam-daniel.bin-zulkornain@etu.univ-orleans.fr>
2024-01-08 23:58:30 +0100

    début partie admin back-end

--- FESTIUT'O/app/views.py
+++ FESTIUT'O/app/views.py
@@ -2,7 +2,7 @@
 import os
 from .app import app, db
 from .models import *
-# from .forms import LoginForm, UtilisateurForm, UserForm, CommandeForm, MaterielForm, MaterielModificationForm, MaterielInstanceForm
+from .forms import *
 
 from flask import jsonify, render_template, send_from_directory, url_for, redirect, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
@@ -15,3 +15,13 @@ def home():
     s = Spectateur.query.all()
     a = Artiste.query.all()
     return render_template("home.html", spectateurs=s, artistes=a)
+
+@app.route("/admin/ajout-billet/")
+def ajout_billet():
+    f = BilletForm()
+    return render_template("ajout-billet.html", form=f)
+
+@app.route("/admin/ajout-spectateur/")
+def ajout_spectateur():
+    f = SpectateurForm()
+    return render_template("ajout-billet.html", form=f)
