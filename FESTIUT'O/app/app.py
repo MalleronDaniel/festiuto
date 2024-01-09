cf49005b7054a1bf0f7e139568855e2cb5dbf745
Kevin-LB <le.breton.kevin@icloud.com>
2024-01-09 23:03:18 +0100

    modif acceuil

--- FESTIUT'O/app/app.py
+++ FESTIUT'O/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 # from flask_login import LoginManager
 
@@ -13,10 +14,18 @@ app.config["SECRET_KEY"] = "3111990a-e74c-4366-8f1e-77c770304a87"  # TODO
 # host = 'localhost'
 # database = 'BD'
 
-username = 'rooot'
-password = 'root'
+# username = 'rooot'
+# password = 'root'
+# host = 'localhost'
+# database = 'festiuto2'
+
+username = 'root'
+password = ''
 host = 'localhost'
-database = 'festiuto2'
+database = 'cours'
+
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+bootstrap = Bootstrap5(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+username+':'+password+'@'+host+'/'+database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
